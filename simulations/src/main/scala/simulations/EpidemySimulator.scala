b0d9e1baf222b4da980b6e44e93d76058162b63e
Tom Sisk <tsisk1985@hotmail.com>
2013-11-19 23:06:12 +1100

    Get the basic Epidemy Simulator up and running

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -10,19 +10,26 @@ class EpidemySimulator extends Simulator {
     val population: Int = 300
     val roomRows: Int = 8
     val roomColumns: Int = 8
-
-    // to complete: additional parameters of simulation
+    val prevalenceRate: Double = 0.01
+    val deathRate: Int = 25
+    val transmissibilityRate: Int = 40
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  var initialInfected = (population * prevalenceRate).toInt
+  val persons: List[Person] = (1 to 300).map(new Person(_)).toList
+
 
   class Person (val id: Int) {
-    var infected = false
+    import RoomHelpers._
+
+    private var innerInfected = false
+    infected = if (initialInfected > 0) { initialInfected -= 1; true; } else false
     var sick = false
     var immune = false
     var dead = false
+    var daysInfected = 0
 
     // demonstrates random number generation
     var row: Int = randomBelow(roomRows)
@@ -31,5 +38,105 @@ class EpidemySimulator extends Simulator {
     //
     // to complete with simulation logic
     //
+
+    // Do an initial move.
+    action()
+
+    def infected_=(newValue: Boolean) {
+      val oldValue = innerInfected
+      innerInfected = newValue
+      if (!oldValue && newValue) {
+        // We are newly infected
+        afterDelay(1){ updateInfectionState() }
+      }
+    }
+
+    def infected = innerInfected
+
+    def action() {
+      afterDelay(randomBelow(5) + 1){
+        if (!dead) {
+          move()
+          action()
+        }
+      }
+    }
+
+    def move() = findRoomToMoveTo(row, col) match {
+      case Some((r, c)) => moveToRoom(r, c)
+      case None => // Zombies everywhere!
+    }
+
+    def moveToRoom(r: Int, c: Int) = {
+      row = r
+      col = c
+      if (!infected && !immune && doesRoomContainInfectedPeople(row, col)) {
+        infected = rollTheDie(transmissibilityRate)
+      }
+    }
+
+    def updateInfectionState(): Unit = {
+      daysInfected += 1
+
+      // I am infected. Am I going to die?
+      if (daysInfected == 18) {
+        immune = false
+        infected = false
+        daysInfected = 0
+      } else {
+        if (daysInfected == 16) {
+          sick = false
+          immune = true
+        } else if (daysInfected == 14) {
+          dead = rollTheDie(deathRate)
+        } else if (daysInfected == 6) {
+          sick = true
+        }
+
+        if (!dead) afterDelay(1){ updateInfectionState() }
+      }
+    }
+
+    def rollTheDie(rate: Int) = (randomBelow(100) + 1) <= rate
+  }
+
+  object RoomHelpers {
+    import RoomNavigationHelpers._
+
+    def doesRoomContainInfectedPeople(room: (Int, Int)) =
+      doesRoomContainPeopleMeetingPredicate(room)(p => p.infected)
+
+    def doesRoomContainVisiblyInfectedPeople(room: (Int, Int)) =
+      doesRoomContainPeopleMeetingPredicate(room)(p => p.sick || p.dead)
+
+    def doesRoomContainPeopleMeetingPredicate(room: (Int, Int))(pred: Person => Boolean) =
+      persons.filter(p => p.row == room._1 && p.col == room._2).exists(pred)
+
+    val possibleRooms = List[(Int, Int) => (Int, Int)](getNorth, getSouth, getEast, getWest)
+    def findRoomToMoveTo(room: (Int, Int)): Option[(Int, Int)] = {
+      val rooms = possibleRooms.map(_(room._1, room._2)).filterNot(doesRoomContainVisiblyInfectedPeople)
+      if (rooms.isEmpty)
+        None
+      else
+        Some(rooms(randomBelow(rooms.length)))
+    }
+  }
+
+  object RoomNavigationHelpers {
+    def getNorth(r: Int, c: Int): (Int, Int) = {
+      (if (r - 1 < 0) roomRows - 1 else r - 1, c)
+    }
+
+    def getSouth(r: Int, c: Int): (Int, Int) = {
+      ((r + 1) % roomRows, c)
+    }
+
+    def getEast(r: Int, c: Int): (Int, Int) = {
+      (r, (c + 1) % roomColumns)
+    }
+
+    def getWest(r: Int, c: Int): (Int, Int) = {
+      (r, if (c - 1 < 0) roomColumns - 1 else c - 1)
+    }
   }
 }
