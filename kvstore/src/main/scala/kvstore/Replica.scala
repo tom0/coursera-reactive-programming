436777f93a7b5038d29efa1073431d2bd94ed859
Tom Sisk <tom.sisk@outlook.com>
2014-01-02 05:19:59 +0000

    step 5, finally

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -50,74 +50,104 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
-  var persists = Map.empty[Long, (ActorRef, Persist)]
+  var persists = Map.empty[Long, (ActorRef, Set[Persist], Set[ActorRef])]
 
   val persistenceActor = context.actorOf(persistenceProps)
 
   context.system.scheduler.schedule(0 milliseconds, 100 milliseconds) {
     persists foreach {
-      case (id, (_, persist)) => persistenceActor ! persist
+      case (id, (_, persist, _)) => persist.foreach { p =>
+        println("PersistRetry :: Sending Persist: " + p)
+        persistenceActor ! p 
+      }
     }
   }
 
   arbiter ! Join
 
   def receive = {
-    case JoinedPrimary   => context.become(leader(Map.empty[Long, (ActorRef, Set[ActorRef])]))
-    case JoinedSecondary => context.become(replica(0L))
+    case JoinedPrimary   => 
+      println("Leader :: Got JoinedPrimary")
+      context.become(leader)
+    case JoinedSecondary =>
+      println("Replica :: Got JoinedSecondary")
+      context.become(replica(0L))
   }
 
   /* TODO Behavior for  the leader role. */
-  def leader(replicationsInProgress: Map[Long, (ActorRef, Set[ActorRef])]): Receive = {
+  def leader: Receive = {
     case r @ Replicas(newReps) =>
       println("Leader :: Got Replicas: " + r)
-      // Not amazingly efficient...
       val existingReps = secondaries.keys.toSet
       val addedReps = newReps -- existingReps
       val removedReps = existingReps -- newReps
+      println("\tAdded count: " + addedReps.size)
+      println("\tRemvd count: " + removedReps.size)
       if (addedReps.nonEmpty) {
-        secondaries = secondaries ++
-          (addedReps zip (addedReps map { _ =>
-            // TODO: Does this replicator need to be started in some way?
-            // TODO: The replicator needs to be told about all KVPs at this point,
-            // TODO: so that it's state is consistent with the rest of the system.
-            context.actorOf(Props(new Replicator(self)))
-          }))
+        addedReps.filter(addedRep => addedRep != self).foreach { addedRep =>
+          // TODO: The replicator needs to be told about all KVPs at this point,
+          // TODO: so that it's state is consistent with the rest of the system.
+          secondaries += (addedRep -> context.actorOf(Replicator.props(addedRep)))
+        }
       }
       if (removedReps.nonEmpty) {
         // TODO: The removed replicators must be terminated
         removedReps.foreach { removedRep => secondaries = secondaries - removedRep }
       }
+      println("Leader :: After add/remove. secondaries: " + secondaries)
       replicators = secondaries.values.toSet
-    case Insert(key, value, id) =>
+
+    case i @ Insert(key, value, id) =>
+      println("Leader :: Got Insert: " + i)
       kv += key -> value
-      persist(id, Persist(key, Some(value), id), sender)
-      context.become(leader(replicationsInProgress + (id -> replicate(Replicate(key, Some(value), id), replicators, sender))))
-    case Remove(key, id) =>
+      val p = Persist(key, Some(value), id)
+      println("Leader :: Persisting: " + p)
+      persist(id, p, sender)
+      val r = Replicate(key, Some(value), id)
+      println("Leader :: Replicating: " + r)
+      replicate(id, Replicate(key, Some(value), id), replicators, sender)
+
+    case rem @ Remove(key, id) =>
+      println("Leader :: Got Remove: " + rem)
       kv = kv - key
-      persist(id, Persist(key, None, id), sender)
-      context.become(leader(replicationsInProgress + (id -> replicate(Replicate(key, None, id), replicators, sender))))
-    case Get(key, id) =>
+      val p = Persist(key, None, id)
+      println("Leader :: Persisting: " + p)
+      persist(id, p, sender)
+      val r = Replicate(key, None, id)
+      println("Leader :: Replicating: " + r)
+      replicate(id, r, replicators, sender)
+
+    case g @ Get(key, id) =>
+      println("Leader :: Got Get: " + g)
       sender ! GetResult(key, kv.get(key), id)
-    case Persisted(key, id) =>
+
+    case p @ Persisted(key, id) =>
+      println("Leader :: Got Persisted: " + p)
       persists.get(id).foreach {
-        case (originalSender, _) =>
-          persists -= id
-          if (replicationsInProgress.get(id).isEmpty) originalSender ! OperationAck(id)
+        case (originalSender, _, reps) =>
+          // If the ID is present, and the replicator set is empty (i.e. replication is complete), then we are good to ack.
+          // There is only ever one entry in the persistence set, so don't bother removing the persistence item, just remove
+          // the entire entry from the map.
+          if (reps.isEmpty) {
+            persists -= id
+            originalSender ! OperationAck(id)
+          } else {
+            persists += (id -> (originalSender, Set.empty[Persist], reps))
+          }
+        case _ =>
       }
+
     case r @ Replicated(key, id) =>
       println("Leader :: Got Replicated: " + r)
-      replicationsInProgress.get(id).foreach {
-        case (s, replicatorActors) =>
+      persists.get(id).foreach {
+        case (s, ps, replicatorActors) =>
           val replicatorsStillWaitingFor = replicatorActors - sender
-          if (replicatorsStillWaitingFor.isEmpty) {
-            // There are now no pending replications for this ID, so remove it.
-            context.become(leader(replicationsInProgress - id))
-            if (!persists.contains(id)) {
-              s ! OperationAck(id)
-            }
+          if (replicatorsStillWaitingFor.isEmpty && ps.isEmpty) {
+            // Not waiting for any replicators or persistence, so ack.
+            persists -= id
+            s ! OperationAck(id)
           } else {
-            context.become(leader(replicationsInProgress + (id -> (s, replicatorsStillWaitingFor))))
+            persists += id -> (s, ps, replicatorsStillWaitingFor)
           }
       }
   }
@@ -134,43 +164,60 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
       println("Replica :: Got Snapshot: " + s)
       kv += key -> value
       context.become(replica(getNextSeq(expectedSeq, seq)))
-      persist(seq, Persist(key, Some(value), seq), sender)
+      val p = Persist(key, Some(value), seq)
+      println("Replica :: Persisting: " + p)
+      persist(seq, p, sender)
     case s @ Snapshot(key, None, seq) =>
       println("Replica :: Got Snapshot: " + s)
       kv -= key
       context.become(replica(getNextSeq(expectedSeq, seq)))
-      persist(seq, Persist(key, None, seq), sender)
+      val p = Persist(key, None, seq)
+      println("Replica :: Persisting: " + p)
+      persist(seq, p, sender)
     case Get(key, id) =>
       sender ! GetResult(key, kv.get(key), id)
     case p @ Persisted(key, id) =>
       println("Replica :: Got Persisted: " + p)
       persists.get(id).foreach {
-        case (originalSender, _) =>
+        case (originalSender, _, _) =>
+          // Update the map to clear the persist.
+          // replica nodes don't do replication.
           persists -= id
           val s = SnapshotAck(key, id)
           println("Replica :: Sending SnapshotAck: " + s)
           originalSender ! s
       }
+    case x => println("Replica :: Got unknown message: " + x)
   }
 
-  def replicate(replicate: Replicate, replicators: Set[ActorRef], s: ActorRef) = {
-    println("Replica :: Replicating: " + replicate)
+  def replicate(id: Long, replicate: Replicate, replicators: Set[ActorRef], s: ActorRef) = {
+
+    // There has to be a nice way of getting a tuple from a map, and then selecting an element for that tuple, 
+    // while providing a default for the *element* if the tuple is not present in the map?
+    val persist = if (persists.contains(id)) persists.get(id).map(_._2).head else Set.empty[Persist]
+
+    persists += (id -> (s, persist, replicators))
     replicators.foreach { replicator =>
       replicator ! replicate
     }
 
-    // If complete replication is not complete in a second,
+    // If complete replication is not finished in a second,
     // then fail.
-//    context.system.scheduler.scheduleOnce(1 second) {
-//      s ! OperationFailed(id)
-//    }
-
-    (s, replicators)
+    context.system.scheduler.scheduleOnce(1 second) {
+      if (persists.contains(id)) {
+        //persists -= id
+        s ! OperationFailed(id)
+      }
+    }
   }
 
   def persist(id: Long, persist: Persist, s: ActorRef) {
-    println("Replica :: Persisting: " + persist)
-    persists += id -> (s, persist)
+
+    // There has to be a nice way of getting a tuple from a map, and then selecting an element for that tuple, 
+    // while providing a default for the *element* if the tuple is not present in the map?
+    val replicators = if (persists.contains(id)) persists.get(id).map(_._3).head else Set.empty[ActorRef]
+
+    persists += (id -> (s, Set(persist), replicators))
     persistenceActor ! persist
 
     // If nothing is heard from the persistenceActor within
@@ -178,6 +225,7 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
     context.system.scheduler.scheduleOnce(1 second) {
       if (persists.contains(id)) {
         persists -= id
+        // The secondaries actually aren't meant to send anything, but it doesn't hurt... ahem.
         s ! OperationFailed(id)
       }
     }
