5417193d08a1efbde4eee044b94e6f7513032870
tom <tom@Yarrrrp>
2014-03-07 16:46:57 +1100

    Move global replication out into separate actor. Greatly simplifies the PrimaryReplica.

--- kvstore-2/src/main/scala/kvstore/PrimaryReplica.scala
+++ kvstore-2/src/main/scala/kvstore/PrimaryReplica.scala
@@ -4,19 +4,21 @@ import akka.actor.{ActorRef, Actor, Props}
 import kvstore.Replica._
 import kvstore.Replica.OperationFailed
 import kvstore.Replica.Remove
-import kvstore.Replicator.{Replicated, Replicate}
+import kvstore.Replicator.Replicate
 import kvstore.Persistence.Persist
 import scala.Some
 import kvstore.Replica.OperationAck
 import kvstore.Arbiter.Replicas
 import kvstore.Replica.Insert
+import kvstore.Persister.{PersistFailed, PersistComplete}
+import kvstore.GlobalReplicator.{GlobalReplicationTimedOut, GlobalReplicationSuccess, ReplicatorsRemoved}
 
 class PrimaryReplica(persistenceProps: Props) extends Actor {
 
-  var replicateId = 0L
-  def nextReplicateId = {
-    val ret = replicateId
-    replicateId += 1
+  var replicateAndPersistId = 0L
+  def nextReplicateAndPersistId = {
+    val ret = replicateAndPersistId
+    replicateAndPersistId += 1
     ret
   }
 
@@ -25,193 +27,162 @@ class PrimaryReplica(persistenceProps: Props) extends Actor {
 
   def receive: Receive = { case _ => }
 
-  type ReplicateId = Long
-  type OriginalRequestId = Long
-  type OriginalClient = ActorRef
-  type SecondaryReplica = ActorRef
-  type SecondaryReplicator = ActorRef
-
   def properReceive(
     kv: Map[String, String] = Map.empty[String, String],
-    pendingPersists: Set[OriginalRequestId] = Set.empty[OriginalRequestId],
-    replicasAndReplicators: Map[SecondaryReplica, SecondaryReplicator] = Map.empty[SecondaryReplica, SecondaryReplicator],
-    replicasToReplicateIds: Map[SecondaryReplica, Set[ReplicateId]] = Map.empty[SecondaryReplica, Set[ReplicateId]],
-    replicateIdsToOriginalRequestIds: Map[ReplicateId, OriginalRequestId] = Map.empty[ReplicateId, OriginalRequestId],
-    pendingReplications: Map[OriginalRequestId, (OriginalClient, Map[ReplicateId, SecondaryReplica])]
-      = Map.empty[OriginalRequestId, (OriginalClient, Map[ReplicateId, SecondaryReplica])]): akka.actor.Actor.Receive = {
+    replicasAndReplicators: Map[ActorRef, ActorRef] = Map.empty[ActorRef, ActorRef], // Replica -> Replicator
+    pendingPersists: Set[Long] = Set.empty[Long],
+    pendingReplications: Map[Long, ActorRef] = Map.empty[Long, ActorRef], // ReplicateAndPersistId -> GlobalReplicator
+    originalClients: Map[Long, (Long, ActorRef)] = Map.empty[Long, (Long, ActorRef)]): akka.actor.Actor.Receive = {
 
     case Insert(key, value, id) =>
-      // Starting a new Persister will commence persistence.
-      // The persister will stop itself on success OR failure.
-      context.actorOf(Props(new Persister(new Persist(key, Some(value), id), persistenceActor)))
+      val replicateAndPersistId = nextReplicateAndPersistId
+
       val newKv = kv + (key -> value)
-      val newPendingPersists = pendingPersists + id
 
-      val (newReplicasToReplicateIds, newReplicateIdsToOriginalRequestIds, newPendingReplications) =
-        replicate(key, Some(value), id, replicasAndReplicators, replicasToReplicateIds, replicateIdsToOriginalRequestIds, pendingReplications)
+      context.actorOf(Props(new Persister(new Persist(key, Some(value), replicateAndPersistId), persistenceActor)))
+      val newPendingPersists = pendingPersists + replicateAndPersistId
 
-      context.become(
-        properReceive(
-          newKv,
-          newPendingPersists,
-          replicasAndReplicators,
-          newReplicasToReplicateIds,
-          newReplicateIdsToOriginalRequestIds,
-          newPendingReplications))
-
-    case Remove(key, id) =>
-      context.actorOf(Props(new Persister(new Persist(key, None, id), persistenceActor)))
-      val newKv = kv - key
-      val newPendingPersists = pendingPersists + id
-
-      val (newReplicasToReplicateIds, newReplicateIdsToOriginalRequestIds, newPendingReplications) =
-        replicate(key, None, id, replicasAndReplicators, replicasToReplicateIds, replicateIdsToOriginalRequestIds, pendingReplications)
-
-      context.become(
-        properReceive(
-          newKv,
-          newPendingPersists,
-          replicasAndReplicators,
-          newReplicasToReplicateIds,
-          newReplicateIdsToOriginalRequestIds,
-          newPendingReplications))
-
-    case Persister.PersistComplete(key, seq) =>
-
-      // Check if replication has finished.
-      val finishedReplication =
-        pendingReplications
-          .get(seq)
-          .filter {
-            case (originalClient, replicateIdToReplica) => replicateIdToReplica.isEmpty
-          }
-
-      // Send the ack if replication has finished.
-      finishedReplication
-        .foreach {
-          case (originalClient, _) => originalClient ! OperationAck(seq)
+      val newPendingReplications =
+        if (replicasAndReplicators.nonEmpty) {
+          val globalReplicator =
+            context.actorOf(Props(new GlobalReplicator(new Replicate(key, Some(value), replicateAndPersistId), replicasAndReplicators.values.toSet)))
+          pendingReplications + (replicateAndPersistId -> globalReplicator)
+        } else {
+          pendingReplications
         }
 
-      // Remove the pending replication if necessary.
-      val newPendingReplications = if (finishedReplication.isEmpty) pendingReplications else pendingReplications - seq
+      val newOriginalClients = originalClients + (replicateAndPersistId -> (id -> sender))
 
-      context.become(
-        properReceive(
-          kv,
-          pendingPersists - seq,
-          replicasAndReplicators,
-          replicasToReplicateIds,
-          replicateIdsToOriginalRequestIds,
-          newPendingReplications))
+      context.become(properReceive(newKv, replicasAndReplicators, newPendingPersists, newPendingReplications, newOriginalClients))
 
-    case Persister.PersistFailed(key, seq) =>
-      val outstandingReplications = pendingReplications.get(seq)
+    case Remove(key, id) =>
+      val replicateAndPersistId = nextReplicateAndPersistId
 
-      val replicateIdsToReplicas = outstandingReplications.map {
-        case (originalClient, mapOfReplicateIdsToReplicas) => mapOfReplicateIdsToReplicas
-      }.getOrElse(Map.empty[ReplicateId, SecondaryReplica])
+      val newKv = kv - key
 
-      // Get the updated values for replicasToReplicateIds (i.e. without this replicateId).
-      val updatedReplicasToReplicateIds = replicateIdsToReplicas.map {
-        case (rId, secondaryReplica) => (secondaryReplica, replicasToReplicateIds.getOrElse(secondaryReplica, Set.empty[ReplicateId]) - rId)
+      context.actorOf(Props(new Persister(new Persist(key, None, replicateAndPersistId), persistenceActor)))
+      val newPendingPersists = pendingPersists + replicateAndPersistId
+
+      val newPendingReplications =
+        if (replicasAndReplicators.nonEmpty) {
+          val globalReplicator =
+            context.actorOf(Props(new GlobalReplicator(new Replicate(key, None, replicateAndPersistId), replicasAndReplicators.values.toSet)))
+          pendingReplications + (replicateAndPersistId -> globalReplicator)
+        } else {
+          pendingReplications
+        }
+
+      val newOriginalClients = originalClients + (replicateAndPersistId -> (id -> sender))
+
+      context.become(properReceive(newKv, replicasAndReplicators, newPendingPersists, newPendingReplications, newOriginalClients))
+
+    case PersistComplete(key, id) =>
+      val newPendingPersists = pendingPersists - id
+
+      // Ensure that we are not still replicating this update.
+      val newOriginalClients =
+        if (!pendingReplications.contains(id)) {
+          originalClients.get(id).foreach {
+            case (originalId, originalClient) =>
+              originalClient ! OperationAck(originalId)
+          }
+          originalClients - id
+        } else {
+          originalClients
+        }
+
+      context.become(properReceive(kv, replicasAndReplicators, newPendingPersists, pendingReplications, newOriginalClients))
+
+    case PersistFailed(key, id) =>
+      val newPendingPersists = pendingPersists - id
+
+      // Must also update the replications, so that we don't send an ack in the future.
+      val newPendingReplications = pendingReplications - id
+
+      originalClients.get(id).foreach {
+        case (originalId, originalClient) => originalClient ! OperationFailed(originalId)
       }
 
-      // Out of the updated values, which are empty?
-      val emptyReplicasToReplicateIds = updatedReplicasToReplicateIds.filter {
-        case (_, rId) => rId.isEmpty
-      }.map {
-        case (secondaryReplica, _) => secondaryReplica
-      }
-
-      // Use the updated and empty values to update and remove respectively.
-      val newReplicasToReplicateIds = (replicasToReplicateIds ++ updatedReplicasToReplicateIds) -- emptyReplicasToReplicateIds
-      val newReplicateIdsToOriginalRequestIds = replicateIdsToOriginalRequestIds -- replicateIdsToReplicas.keySet
-      val newPendingReplications = pendingReplications - seq
-
-      context.become(
-        properReceive(
-          kv,
-          pendingPersists - seq,
-          replicasAndReplicators,
-          newReplicasToReplicateIds,
-          newReplicateIdsToOriginalRequestIds,
-          newPendingReplications))
-
-      outstandingReplications.foreach {
-        case (originalClient, _) => originalClient ! OperationFailed(seq)
-      }
+      context.become(properReceive(kv, replicasAndReplicators, newPendingPersists, newPendingReplications, originalClients - id))
 
     case Get(key, id) =>
       sender ! GetResult(key, kv.get(key), id)
 
-//    case Replicas(newReplicas) =>
-//      val newReplicasWithoutSelf = newReplicas.filter( _ != self )
-//      val currentReplicas = replicasAndReplicators.keySet
-//      val addedReplicas = newReplicasWithoutSelf.diff(currentReplicas)
-//      val removedReplicas = currentReplicas.diff(newReplicasWithoutSelf)
-//
-//      val addedReplicasAndReplicators = addedReplicas.map(r => r -> context.actorOf(Props(new Replicator(r))))
-//
-//      val replicationsToSend =
-//        for {
-//          addedReplicaAndReplicator <- addedReplicasAndReplicators
-//          kvPair <- kv
-//        } yield Replicate(kvPair._1, Some(kvPair._2), nextReplicateId)
-//
-//      // Set the new state.
-//      val newReplicasAndReplicators = (replicasAndReplicators -- removedReplicas) ++ addedReplicasAndReplicators
-//      val newPendingReplications = pendingReplications ++ replicationsToSend
-//      context.become(receive(kv, newReplicasAndReplicators, newPendingReplications))
-//
-//    case Replicated(key, id) =>
-//      val originalRequestId =
-//        replicateIdsToOriginalRequestIds
-//          .get(id)
-//          .foreach {
-//            case (originalClient, setOfReplicateIds) =>
-//              // TODO: Check if the change has been persisted.
-//          }
+    case Replicas(newReplicas) =>
+      val newReplicasWithoutSelf = newReplicas.filter( _ != self )
+      val currentReplicas = replicasAndReplicators.keySet
+      val addedReplicas = newReplicasWithoutSelf.diff(currentReplicas)
+      val removedReplicas = currentReplicas.diff(newReplicasWithoutSelf)
+
+      // We need to
+      // 1. Allocate replicators for the newly added replicas
+      // 2. Send replication messages to all of the new replicas
+      // 3. Stop the replicators that have been removed
+      // 4. Send messages to all the current replications telling them about the removed replicas.
+      // 5. Update all of the state, and context.become.
+
+      // 1. Allocate replicators for the newly added replicas
+      val addedReplicasAndReplicators = addedReplicas.map {
+        addedReplica => (addedReplica, context.actorOf(Props(new Replicator(addedReplica))))
+      }.toMap
+
+
+      // 2. Send replication messages to all of the new replicas
+      if (addedReplicasAndReplicators.nonEmpty) {
+        kv.foreach {
+          case (key, value) =>
+            val replicateId = nextReplicateAndPersistId
+            val replicate = new Replicate(key, Some(value), replicateId)
+            context.actorOf(Props(new GlobalReplicator(replicate, addedReplicasAndReplicators.values.toSet)))
+        }
+      }
+
+      // 3. Stop the replicators that have been removed
+      val removedReplicators =
+        removedReplicas
+          .map(removedRep => replicasAndReplicators.get(removedRep))
+          .filter(removedReplicator => removedReplicator.nonEmpty)
+          .map(nonEmptyRemovedReplicator => nonEmptyRemovedReplicator.get)
+
+      removedReplicators.foreach(removedReplicator => context.stop(removedReplicator))
+
+      // 4. Send messages to all the current replications telling them about the removed replicas.
+      pendingReplications.foreach {
+        case (_, globalReplicator) => globalReplicator ! ReplicatorsRemoved(removedReplicators)
+      }
+
+      // 5. Update all of the state, and context.become.
+      val newReplicasAndReplicators = (replicasAndReplicators ++ addedReplicasAndReplicators) -- removedReplicas
+      context.become(properReceive(kv, newReplicasAndReplicators, pendingPersists, pendingReplications, originalClients))
+
+    case GlobalReplicationSuccess(replicate: Replicate) =>
+      val newPendingReplications = pendingReplications - replicate.id
+
+      // Ensure that we are not still persisting this update.
+      val newOriginalClients =
+        if (!pendingPersists.contains(replicate.id)) {
+          originalClients.get(replicate.id).foreach {
+            case (originalId, originalClient) => originalClient ! OperationAck(originalId)
+          }
+          originalClients - replicate.id
+        } else {
+          originalClients
+        }
+
+      context.become(properReceive(kv, replicasAndReplicators, pendingPersists, newPendingReplications, newOriginalClients))
+
+    case GlobalReplicationTimedOut(replicate: Replicate) =>
+      val newPendingReplications = pendingReplications - replicate.id
+
+      // Must also update the persists, so that we don't send an ack in the future.
+      val newPendingPersists = pendingPersists - replicate.id
+
+      originalClients.get(replicate.id).foreach {
+        case (originalId, originalClient) => originalClient ! OperationFailed(originalId)
+      }
+
+      context.become(properReceive(kv, replicasAndReplicators, newPendingPersists, newPendingReplications, originalClients - replicate.id))
 
     case _ =>
-
-
-  }
-
-
-  def replicate(key: String,
-                value: Option[String],
-                originalRequestId: Long,
-                replicasAndReplicators: Map[SecondaryReplica, ActorRef],
-                replicasToReplicateIds: Map[SecondaryReplica, Set[ReplicateId]],
-                replicateIdsToOriginalRequestIds: Map[ReplicateId, OriginalRequestId],
-                pendingReplications: Map[OriginalRequestId, (OriginalClient, Map[ReplicateId, SecondaryReplica])]):
-    (Map[SecondaryReplica, Set[ReplicateId]],
-     Map[ReplicateId, OriginalRequestId],
-     Map[OriginalRequestId, (OriginalClient, Map[ReplicateId, SecondaryReplica])]) = {
-    // Create the replications to be done.
-    val replications = replicasAndReplicators.map {
-      case (replica, replicator) => (replica, replicator, new Replicate(key, value, nextReplicateId))
-    }
-
-    // Update all of the state.
-    val repToRepId: Iterable[(SecondaryReplica, Set[ReplicateId])] = replications.map {
-      case (replica, _, replicate) => replica -> (replicasToReplicateIds.getOrElse(replica, Set.empty[ReplicateId]) ++ Set(replicate.id))
-    }
-    val newReplicasToReplicateIds = replicasToReplicateIds ++ repToRepId
-
-    val newReplicateIdsToOriginalRequestIds = replicateIdsToOriginalRequestIds ++ replications.map {
-      case (_, _, replicate) => replicate.id -> originalRequestId
-    }
-
-    val newPendingReplications = pendingReplications +
-      (originalRequestId ->
-        (sender, replications.map { case (replica, _, replicate) => replicate.id -> replica }.toMap))
-
-          // Send the replications
-    replications.foreach {
-      case (_, replicator, replicate) => replicator ! replicate
-    }
-
-    (newReplicasToReplicateIds, newReplicateIdsToOriginalRequestIds, newPendingReplications)
   }
 }
